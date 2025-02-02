55007ff075f6cf5b3568c1f795df2004bb4fc118
Kemi <kemistic2021@gmail.com>
2025-02-02 22:22:50 +0000

    help

--- timer.js
+++ timer.js
@@ -1,4 +1,4 @@
-let [ seconds, minutes , hours ]= [ 0,0,0];
+/*let [ seconds, minutes , hours ]= [ 0,0,0];
 let timeRef = document.querySelector("#timer-display");
 let int = null;
 
@@ -37,4 +37,39 @@ function displayTimer(){
     let s = seconds < 10 ? "0" + seconds: seconds;
 
     timeRef.innerHTML = `${h}  : ${m} : ${s}`;
-}
+}*/
+
+
+
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+
+/** @param {number} future */
+
+const timestampDiff = 
+future => 
+    /** @param {number} past */
+past => 
+[HOUR, MINUTE, SECOND].map((time, index, times) => {
+    const diff = future- past;
+    const previousTime = times[index- 1];
+
+    return(
+        Math.floor(diff/time)-
+        (Math.floor(diff/previousTime) * (previousTime/time)||0)
+    );
+});
+/** @param {string} date*/
+
+const countDown = date =>
+    /** @param {HTMLEement} target  */
+target  => {
+    const diff = timestampDiff(Date.parse(date));
+    return  setInterval(() => {
+        const [hours, minutes, seconds] = diff(Date.now()));
+        target.innerHTML = 
+        <><div>${hours}<span>Hours</span></div><div>${minutes}<span>Minutes</span></div><div>${seconds}<span>Seconds</span></div></>
+    };
+    SECOND);
+};
